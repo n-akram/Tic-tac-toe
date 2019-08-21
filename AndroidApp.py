ec0deac1ce214fb1e809dfe5b6bd940d8aea580c
n-akram <31505221+n-akram@users.noreply.github.com>
2019-08-21 13:14:01 +0200

    Android support
    1. Uninstall pycrypto due to mismatch with wheel versions required for kivy
    2. Installed python-for-android to be used for apk generation
    3. Requirement: GNU virtual machine

---
+++ AndroidApp.py
@@ -0,0 +1,12 @@
+'''
+A simple console based tic-tac-toe apk generator
+Note:   1. Uses engine : engine.py
+        2. Uses console.py
+Date: 21.08.2019
+
+source : https://techwithtim.net/tutorials/kivy-tutorial/setup/
+'''
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
